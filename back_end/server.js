d3409bf5a35ab5790be7eabde8f8373a12c4d206
Muawiya Asali <muawiya.asali@gm.com>
2024-01-30 22:43:36 +0200

    adding morgan and nodemon

--- back_end/server.js
+++ back_end/server.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const morgan = require('morgan');
 const app = express();
 const port = 3000;
+
+// Use morgan middleware with the "combined" format
+app.use(morgan('dev'));
 
 app.listen(port, () => {
     console.log('listening on port ' + port);
