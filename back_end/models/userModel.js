53ce115878a0d998bbc8bea6624c16f845204fbd
Muawiya Asali <muawiya.asali@gm.com>
2024-02-01 01:44:21 +0200

    adding basic views

---
+++ back_end/models/userModel.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+// Define the User schema
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    lowercase: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 6, // Minimum password length
+  },
+}, { timestamps: true });
+
+// Hash the user's password before saving to the database
+userSchema.pre('save', async function (next) {
+  const user = this;
+  if (user.isModified('password')) {
+    user.password = await bcrypt.hash(user.password, 10);
+  }
+  next();
+});
+
+// Define the User model
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
--- back_end/server.js
+++ back_end/server.js
@@ -5,11 +5,15 @@ const port = 3000;
 const Poll = require('./models/pollModel');
 
 const mongoose = require('mongoose');
-const dbURI = 'mongodb+srv://muawiyaasali:zAhILtuK7gf0kkPA@pollingappdb.iphhzly.mongodb.net/?retryWrites=true&w=majority'
+const dbURI = 'mongodb+srv://muawiyaasali:zAhILtuK7gf0kkPA@pollingappdb.iphhzly.mongodb.net/pollingAppDB?retryWrites=true&w=majority'
 const clientOptions = { serverApi: { version: '1', strict: true, deprecationErrors: true } };
 
 // Use morgan middleware with the "combined" format
 app.use(morgan('dev'));
+
+// register view engine - it checks views folder by default
+app.set('view engine', 'ejs');
+
 
 mongoose.connect(dbURI, clientOptions)
     .then((result) => app.listen(port, () => {
@@ -17,25 +21,30 @@ mongoose.connect(dbURI, clientOptions)
     }))
     .catch((err) => console.log('error connecting to db' + err));
 
-app.get('/create-poll', (req, res) => {
-    const poll = new Poll({
-        question: 'what is the best programming language?',
-        options: ['python', 'js', 'java'],
-    });
+// static files 
+app.use(express.static('public'));
 
-    poll.save()
-        .then((result) => {
-            res.send(result)
-        })
-        .catch((err) => {
-            console.log(err);
-        });
+app.get('/create-poll', (req, res) => {
+    res.render('new_poll', {});
+    // const poll = new Poll({
+    //     question: 'what is the best programming language?',
+    //     options: ['python', 'js', 'java'],
+    // });
+
+    // poll.save()
+    //     .then((result) => {
+    //         res.send(result)
+    //     })
+    //     .catch((err) => {
+    //         console.log(err);
+    //     });
 });
 
 app.get('/all-polls', (req, res) => {
     Poll.find()
         .then((result) => {
-            res.send(result)
+            // res.send(result)
+            res.render('poll_details', {polls: result});
         })
         .catch((err) => {
             console.log(err);
@@ -50,4 +59,8 @@ app.get('/single-poll', (req, res) => {
     .catch((err) => {
         console.log(err);
     });
+});
+
+app.get('/', (req, res) => {
+    res.render('index');
 });
