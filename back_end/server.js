41445ce42f634d20eab12aaf4d2dc3fe4bf80f95
Muawiya Asali <muawiya.asali@gm.com>
2024-02-02 22:10:29 +0200

    Modifying structure and adding routes

--- back_end/server.js
+++ back_end/server.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const morgan = require('morgan');
 const app = express();
-const port = 3000;
-const Poll = require('./models/pollModel');
 const methodOverride = require('method-override');
 
 const mongoose = require('mongoose');
 const dbURI = 'mongodb+srv://muawiyaasali:zAhILtuK7gf0kkPA@pollingappdb.iphhzly.mongodb.net/pollingAppDB?retryWrites=true&w=majority'
 const clientOptions = { serverApi: { version: '1', strict: true, deprecationErrors: true } };
+
+const poll_routes = require('./routes/poll_routes');
 
 // Use morgan middleware with the "combined" format
 app.use(morgan('dev'));
@@ -21,6 +21,12 @@ app.use(express.urlencoded({extended: true}));
 // Middleware for method override - check delete endoint
 app.use(methodOverride('_method'));
 
+// static files 
+app.use(express.static('public'));
+
+const port = 3000;
+
+
 
 mongoose.connect(dbURI, clientOptions)
     .then((result) => app.listen(port, () => {
@@ -28,84 +34,9 @@ mongoose.connect(dbURI, clientOptions)
     }))
     .catch((err) => console.log('error connecting to db' + err));
 
-// static files 
-app.use(express.static('public'));
-
-app.get('/create-poll', (req, res) => {
-    res.render('new_poll', {});
-    // const poll = new Poll({
-    //     question: 'what is the best programming language?',
-    //     options: ['python', 'js', 'java'],
-    // });
-
-    // poll.save()
-    //     .then((result) => {
-    //         res.send(result)
-    //     })
-    //     .catch((err) => {
-    //         console.log(err);
-    //     });
-});
-
-app.post('/create-poll', (req, res) => {
-    console.log(req.body);
-    const poll = new Poll({
-        question: req.body.title,
-        options: [req.body.option_1, req.body.option_2, 
-            req.body.option_3, req.body.option_4, req.body.option_5],
-    });
-
-    poll.save()
-        .then((result) => {
-            res.redirect('/');
-        })
-        .catch((err) => {
-            console.log(err);
-        });
-});
-
-app.get('/all-polls', (req, res) => {
-    Poll.find().sort({ createdAt: -1})
-        .then((result) => {
-            // res.send(result)
-            res.render('all_polls', {polls: result});
-        })
-        .catch((err) => {
-            console.log(err);
-        });
-});
-
-app.get('/single-poll/:id', (req, res) => {
-    const id = req.params.id;
-    Poll.findById(id)
-    .then((result) => { 
-        // res.send(result)
-        res.render('poll_details', {poll: result});
-    })
-    .catch((err) => {
-        console.log(err);
-    });
-});
-
-app.delete('/delete-poll/:id', async (req, res) => {
-    const id = req.params.id;
-    try {
-        const delete_poll = await Poll.findByIdAndDelete(id);
-
-        if (!delete_poll) {
-            return res.status(404).json({ message: 'Poll not found' });
-          }
-      
-        //   res.json({ message: 'Item deleted successfully', delete_poll });
-        res.render('success', {success_msg: 'Item deleted successfully'});
-
-    } catch (err) {
-        console.log(err);
-        res.status(500).json({ message: 'Internal server error' });
-    }
-    
-});
-
 app.get('/', (req, res) => {
     res.render('index');
 });
+
+// poll routes
+app.use(poll_routes);
