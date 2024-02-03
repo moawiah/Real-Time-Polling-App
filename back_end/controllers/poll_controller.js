68f6aa7da064a2a264b50d6a3986fdb35154a859
Muawiya Asali <muawiya.asali@gm.com>
2024-02-03 15:45:40 +0200

    Implementing voting usecase

--- back_end/controllers/poll_controller.js
+++ back_end/controllers/poll_controller.js
@@ -17,11 +17,73 @@ const create_poll = (req, res) => {
         });
     };
 
+const submit_vote = async (req, res) => {
+    const id = req.params.id;
+    const voting_data = req.body;
+    
+    try {
+        poll = await Poll.findById(id);
+        if (!poll || !voting_data){
+            return res.status(404).send('Poll not found OR Empty Voting Data');
+        }
+
+        console.log(voting_data.selectedOption);
+        console.log(poll.votes);
+        voting_map = poll.votes;
+        voting_choice = voting_data.selectedOption;
+
+        const choice_cnt = voting_map.has(voting_choice);
+        
+        if (!choice_cnt) {
+            voting_map.set(voting_choice, 1);
+            console.log(voting_map);
+        }
+        else {
+            const current_cnt = voting_map.get(voting_choice);
+            voting_map.set(voting_choice, current_cnt+1);
+            console.log(voting_map);
+        }
+
+        await poll.save();
+        res.status(200).send('Vote submitted successfully!');
+
+    } catch (err) {
+        console.log(err);
+        res.status(500).send('Internal Server Error');
+    }
+
+
+
+};
+
 const list_polls = (req, res) => {
     Poll.find().sort({ createdAt: -1})
         .then((result) => {
             // res.send(result)
             res.render('poll/all_polls', {polls: result});
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+};
+
+const list_voting_polls = (req, res) => {
+    Poll.find().sort({ createdAt: -1})
+        .then((result) => {
+            // res.send(result)
+            res.render('poll/poll_voting_list', {polls: result});
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+};
+
+const poll_voting_details = (req, res) => {
+    const id = req.params.id;
+    Poll.findById(id)
+        .then((result) => {
+            // res.send(result)
+            res.render('poll/poll_voting_details', {poll: result});
         })
         .catch((err) => {
             console.log(err);
@@ -68,5 +130,8 @@ module.exports = {
     list_polls,
     get_poll,
     delete_poll,
-    new_poll_form
+    new_poll_form,
+    list_voting_polls,
+    poll_voting_details,
+    submit_vote
 }
--- back_end/routes/poll_routes.js
+++ back_end/routes/poll_routes.js
@@ -12,4 +12,10 @@ router.get('/single-poll/:id', poll_controller.get_poll);
 
 router.delete('/delete-poll/:id', poll_controller.delete_poll);
 
+router.get('/poll_voting_list', poll_controller.list_voting_polls);
+
+router.get('/poll_voting_details/:id', poll_controller.poll_voting_details);
+
+router.post('/submit-vote/:id', poll_controller.submit_vote);
+
 module.exports = router;
